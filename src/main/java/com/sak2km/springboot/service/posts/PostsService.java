9bd1da80fba3370028eef028d153d9a568362489
sonwookim <sak2km@gmail.com>
2021-08-19 03:28:54 -0400

    chapter 4

--- src/main/java/com/sak2km/springboot/service/posts/PostsService.java
+++ src/main/java/com/sak2km/springboot/service/posts/PostsService.java
@@ -2,6 +2,7 @@ package com.sak2km.springboot.service.posts;
 
 import com.sak2km.springboot.domain.posts.Posts;
 import com.sak2km.springboot.domain.posts.PostsRepository;
+import com.sak2km.springboot.web.dto.PostsListResponseDto;
 import com.sak2km.springboot.web.dto.PostsResponseDto;
 import com.sak2km.springboot.web.dto.PostsSaveRequestDto;
 import com.sak2km.springboot.web.dto.PostsUpdateRequestDto;
@@ -9,6 +10,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -31,5 +34,19 @@ public class PostsService {
     public PostsResponseDto findById(Long id) {
         Posts entity = postsRepository.findById(id).orElseThrow(()-> new IllegalArgumentException("No post exists. id=" + id));
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void delete(Long id) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("No post exists with id: " + id));
+        postsRepository.delete(posts);
     }
 }
